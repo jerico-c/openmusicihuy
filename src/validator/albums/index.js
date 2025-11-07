e31cc3446d334a64bf8a85b3f23ff7e92e501dd8
Jerico Christianto <jericochristianto@gmail.com>
2025-11-07 23:17:06 +0700

    Add files via upload

--- src/validator/albums/index.js
+++ src/validator/albums/index.js
@@ -1,14 +1,23 @@
-// src/validator/albums/index.js
-const { AlbumPayloadSchema } = require('./schema');
+const { AlbumPayloadSchema, ImageHeadersSchema } = require('./schema');
 const InvariantError = require('../../exceptions/InvariantError');
 
 const AlbumsValidator = {
-  validateAlbumPayload: (payload) => {
-    const validationResult = AlbumPayloadSchema.validate(payload);
-    if (validationResult.error) {
-      throw new InvariantError(validationResult.error.message);
-    }
-  },
+    validateAlbumPayload: (payload) => {
+        const validationResult = AlbumPayloadSchema.validate(payload);
+
+        if (validationResult.error) {
+            throw new InvariantError(validationResult.error.message);
+        }
+    },
+
+    validateCoverAlbumImageHeaders: (headers) => {
+        const validationResult = ImageHeadersSchema.validate(headers);
+
+        if (validationResult.error) {
+            console.log(validationResult.error.message, 'validationResult.error.message');
+            throw new InvariantError(validationResult.error.message);
+        }
+    },
 };
 
 module.exports = AlbumsValidator;
--- src/validator/albums/schema.js
+++ src/validator/albums/schema.js
@@ -1,9 +1,26 @@
-// src/validator/albums/schema.js
-const Joi = require('@hapi/joi');
+const Joi = require('joi');
 
 const AlbumPayloadSchema = Joi.object({
-  name: Joi.string().required(),
-  year: Joi.number().required(),
+    name: Joi.string().required(),
+    year: Joi.number()
+        .integer()
+        .min(1900)
+        .max(2030)
+        .required(),
 });
 
-module.exports = { AlbumPayloadSchema };
+const ImageHeadersSchema = Joi.object({
+    'content-type': Joi.string()
+        .valid(
+            'image/apng',
+            'image/avif',
+            'image/gif',
+            'image/jpeg',
+            'image/png',
+            'image/svg+xml',
+            'image/webp'
+        )
+        .required(),
+}).unknown();
+
+module.exports = { AlbumPayloadSchema, ImageHeadersSchema };
