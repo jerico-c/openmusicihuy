de7bda75f1006ef66b808fb804484b43a715eaa9
Jerico Christianto <jericochristianto@gmail.com>
2025-11-07 23:18:00 +0700

    Add files via upload

--- src/validator/songs/index.js
+++ src/validator/songs/index.js
@@ -1,14 +1,22 @@
-// src/validator/songs/index.js
-const { SongPayloadSchema } = require('./schema');
+const { SongPayloadSchema, SongQuerySchema } = require('./schema');
 const InvariantError = require('../../exceptions/InvariantError');
 
 const SongsValidator = {
-  validateSongPayload: (payload) => {
-    const validationResult = SongPayloadSchema.validate(payload);
-    if (validationResult.error) {
-      throw new InvariantError(validationResult.error.message);
-    }
-  },
+    validateSongPayload: (payload) => {
+        const validationResult = SongPayloadSchema.validate(payload);
+
+        if (validationResult.error) {
+            throw new InvariantError(validationResult.error.message);
+        }
+    },
+
+    validateSongQuery: (payload) => {
+        const validationResult = SongQuerySchema.validate(payload);
+
+        if (validationResult.error) {
+            throw new InvariantError(validationResult.error.message);
+        }
+    },
 };
 
 module.exports = SongsValidator;
--- src/validator/songs/schema.js
+++ src/validator/songs/schema.js
@@ -1,13 +1,20 @@
-// src/validator/songs/schema.js
-const Joi = require('@hapi/joi');
+const Joi = require('joi');
 
 const SongPayloadSchema = Joi.object({
-  title: Joi.string().required(),
-  year: Joi.number().integer().required(),
-  genre: Joi.string().required(),
-  performer: Joi.string().required(),
-  duration: Joi.number().integer(), // Opsional
-  albumId: Joi.string(), // Opsional
+    title: Joi.string().required(),
+    year: Joi.number().integer()
+        .min(1900)
+        .max(2030)
+        .required(),
+    genre: Joi.string().required(),
+    performer: Joi.string().required(),
+    duration: Joi.number().integer(),
+    albumId: Joi.string(),
 });
 
-module.exports = { SongPayloadSchema };
+const SongQuerySchema = Joi.object({
+    title: Joi.string().allow(null, ''),
+    performer: Joi.string().allow(null, ''),
+});
+
+module.exports = { SongPayloadSchema, SongQuerySchema };
