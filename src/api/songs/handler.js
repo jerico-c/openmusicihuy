cbf913146a2a633c853a776b0ea6a795ce05d544
Jerico Christianto <jericochristianto@gmail.com>
2025-11-10 23:31:14 +0700

    Add files via upload

--- src/api/songs/handler.js
+++ src/api/songs/handler.js
@@ -1,84 +1,97 @@
-// src/api/songs/handler.js
-const autoBind = require('auto-bind');
-
 class SongsHandler {
-  constructor(service, validator) {
-    this._service = service;
-    this._validator = validator;
+    constructor(service, validator) {
+        this._service = service;
+        this._validator = validator;
+    }
 
-    autoBind(this);
-  }
+    async postSongHandler(request, h) {
+        this._validator.validateSongPayload(request.payload);
+        const {
+            title = 'untitled',
+            year,
+            genre,
+            performer,
+            duration,
+            albumId,
+        } = request.payload;
 
-  async postSongHandler(request, h) {
-    // Validasi payload
-    this._validator.validateSongPayload(request.payload);
-    
-    // Ambil payload SETELAH divalidasi
-    const { title, year, genre, performer, duration, albumId } = request.payload;
-    
-    // Kirim payload ke service
-    const songId = await this._service.addSong({ title, year, genre, performer, duration, albumId });
+        const songId = await this._service.addSong({
+            title,
+            year,
+            genre,
+            performer,
+            duration,
+            albumId,
+        });
 
-    const response = h.response({
-      status: 'success',
-      data: {
-        songId,
-      },
-    });
-    response.code(201);
-    return response;
-  }
+        const response = h.response({
+            status: 'success',
+            message: 'Lagu berhasil ditambahkan',
+            data: {
+                songId,
+            },
+        });
+        response.code(201);
+        return response;
+    }
 
-  async getSongsHandler(request, h) {
-    const { title, performer } = request.query;
-    const songs = await this._service.getSongs(title, performer);
+    async getSongsHandler(request, h) {
+        const { title = '', performer = '' } = request.query;
+        this._validator.validateSongQuery({ title, performer });
 
-    return {
-      status: 'success',
-      data: {
-        songs,
-      },
-    };
-  }
+        if (title !== '' || performer !== '') {
+            const songs = await this._service.getSongs(title, performer);
+            const response = h.response({
+                status: 'success',
+                data: {
+                    songs,
+                },
+            });
+            return response;
+        }
 
-  async getSongByIdHandler(request, h) {
-    const { id } = request.params;
-    const song = await this._service.getSongById(id);
+        const songs = await this._service.getSongs();
+        return {
+            status: 'success',
+            data: {
+                songs,
+            },
+        };
+    }
 
-    return {
-      status: 'success',
-      data: {
-        song,
-      },
-    };
-  }
+    async getSongByIdHandler(request) {
+        const { id } = request.params;
 
-  async putSongByIdHandler(request, h) {
-    // Validasi payload
-    this._validator.validateSongPayload(request.payload);
+        const song = await this._service.getSongById(id);
 
-    // Ambil payload SETELAH divalidasi
-    const { title, year, genre, performer, duration, albumId } = request.payload;
-    const { id } = request.params;
+        return {
+            status: 'success',
+            data: {
+                song,
+            },
+        };
+    }
 
-    // Kirim payload dan id ke service
-    await this._service.editSongById(id, { title, year, genre, performer, duration, albumId });
+    async putSongByIdHandler(request) {
+        this._validator.validateSongPayload(request.payload);
+        const { id } = request.params;
 
-    return {
-      status: 'success',
-      message: 'Lagu berhasil diperbarui',
-    };
-  }
+        await this._service.editSongById(id, request.payload);
 
-  async deleteSongByIdHandler(request, h) {
-    const { id } = request.params;
-    await this._service.deleteSongById(id);
+        return {
+            status: 'success',
+            message: 'Lagu berhasil diperbarui',
+        };
+    }
 
-    return {
-      status: 'success',
-      message: 'Lagu berhasil dihapus',
-    };
-  }
+    async deleteSongByIdHandler(request) {
+        const { id } = request.params;
+        await this._service.deleteSongById(id);
+        return {
+            status: 'success',
+            message: 'Lagu berhasil dihapus',
+        };
+    }
 }
 
 module.exports = SongsHandler;
--- src/api/songs/routes.js
+++ src/api/songs/routes.js
@@ -1,30 +1,29 @@
-// src/api/songs/routes.js
 const routes = (handler) => [
-  {
-    method: 'POST',
-    path: '/songs',
-    handler: (request, h) => handler.postSongHandler(request, h),
-  },
-  {
-    method: 'GET',
-    path: '/songs',
-    handler: (request, h) => handler.getSongsHandler(request, h),
-  },
-  {
-    method: 'GET',
-    path: '/songs/{id}',
-    handler: (request, h) => handler.getSongByIdHandler(request, h),
-  },
-  {
-    method: 'PUT',
-    path: '/songs/{id}',
-    handler: (request, h) => handler.putSongByIdHandler(request, h),
-  },
-  {
-    method: 'DELETE',
-    path: '/songs/{id}',
-    handler: (request, h) => handler.deleteSongByIdHandler(request, h),
-  },
+    {
+        method: 'POST',
+        path: '/songs',
+        handler: (request, h) => handler.postSongHandler(request, h),
+    },
+    {
+        method: 'GET',
+        path: '/songs',
+        handler: (request, h) => handler.getSongsHandler(request, h),
+    },
+    {
+        method: 'GET',
+        path: '/songs/{id}',
+        handler: (request, h) => handler.getSongByIdHandler(request, h),
+    },
+    {
+        method: 'PUT',
+        path: '/songs/{id}',
+        handler: (request, h) => handler.putSongByIdHandler(request, h),
+    },
+    {
+        method: 'DELETE',
+        path: '/songs/{id}',
+        handler: (request, h) => handler.deleteSongByIdHandler(request, h),
+    },
 ];
 
 module.exports = routes;
