11c43927e8fc9f3656c38c9c1fecf3f9bd52cb6c
Jerico Christianto <jericochristianto@gmail.com>
2025-11-12 09:20:40 +0700

    Add files via upload

--- src/exceptions/InvariantError.js
+++ src/exceptions/InvariantError.js
@@ -1,11 +1,10 @@
-// src/exceptions/InvariantError.js
 const ClientError = require('./ClientError');
 
 class InvariantError extends ClientError {
-  constructor(message) {
-    super(message, 400); // Status code 400
-    this.name = 'InvariantError';
-  }
+    constructor(message) {
+        super(message);
+        this.name = 'InvariantError';
+    }
 }
 
 module.exports = InvariantError;
